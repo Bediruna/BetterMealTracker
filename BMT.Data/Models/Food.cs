14b5ad9e7deb2834b9a1c997e5fc74692044a01d
Bedir Aygun <Bediruna@hotmail.com>
2025-04-19 21:01:21 +0100

    simplifying food model

--- BMT.Data/Models/Food.cs
+++ BMT.Data/Models/Food.cs
@@ -2,27 +2,12 @@ namespace BMT.Data.Models;
 
 public class Food : BaseModel
 {
-    public string Name { get; set; }
-    public string Brand { get; set; }
-    public string Ingredients { get; set; }
+    public string ProductName { get; set; }
     public double Calories { get; set; }
-    public double ProteinG { get; set; }
-    public double FatG { get; set; }
-    public double SaturatedFatG { get; set; }
-    public double TransFatG { get; set; }
-    public double CarbsG { get; set; }
-    public double FiberG { get; set; }
-    public double SugarG { get; set; }
-    public double AddedSugarG { get; set; }
-    public double CholesterolMg { get; set; }
-    public double SodiumMg { get; set; }
-    public double PotassiumMg { get; set; }
-    public double CalciumMg { get; set; }
-    public double IronMg { get; set; }
-    public double VitaminAug { get; set; }
-    public double VitaminCMg { get; set; }
-    public double VitaminDug { get; set; }
-    public double VitaminB12ug { get; set; }
-    public double MagnesiumMg { get; set; }
-    public double ZincMg { get; set; }
+    public double Protein { get; set; }
+    public double Fat { get; set; }
+    public double Carbs { get; set; }
+    public double Sugar { get; set; }
+    public string ServingSize { get; set; }
+    public long LastUsedDate { get; set; }
 }
